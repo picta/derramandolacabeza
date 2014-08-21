c17c9b43388fba2c11f0fb59d818eeeefd750648
Picta Ricta <picta.ricta@gmail.com>
2014-08-21 18:24:31 +0200

    edit, destroy and create removed from production views; unknow routes redirected to index

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'derrames#index'
 
+  get '*path' => redirect('/')
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
